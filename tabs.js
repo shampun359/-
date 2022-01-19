cb24365c1eb6c03a76a2ba7cdee3d9aca76e012a
Mikhail <begasos1997@gmail.com>
2022-01-19 19:24:52 +0400

    Закончил  с  табами

--- tabs.js
+++ tabs.js
@@ -1,12 +1,17 @@
 document.querySelectorAll('#button_tab').forEach(function(tabsBtn){       
     tabsBtn.addEventListener('click', function(event){
         const path = event.currentTarget.dataset.path
-        console.log(path);
 
         document.querySelectorAll('.work_content').forEach(function(tabContent){
             tabContent.classList.remove('tab_contant_active')
         })
         document.querySelector(`[data-target="${path}"]`).classList.add('tab_contant_active')
+
+        document.querySelector('.foto_work').style.background = 'url(img/foto_work_' +  path + '.jpg) no-repeat';
     })
 })
+
+
+
+
 
