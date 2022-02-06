20836d2ca755b0bdfdd8b2bc8a1d002c7fc72034
Mikhail <begasos1997@gmail.com>
2022-02-06 14:43:38 +0400

    Gulp
    Автофпрефиксы для  CSS

--- gulp-project/gulpfile.js
+++ gulp-project/gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');   /*Подключаем галп*/
 const cleanCSS = require('gulp-clean-css');  /*Плагин для минификации  CSS*/
 const uglify = require('gulp-uglify'); /*Плагин для минификации  JS*/
 var pipeline = require('readable-stream').pipeline; /*Плагин для минификации  JS (дополнительный)*/
+const autoprefixer = require('gulp-autoprefixer'); /*авто префиксы*/
 
 gulp.task('hello', function(callback){
     console.log("Hello");
@@ -19,10 +20,21 @@ gulp.task('minifyCSS', function() {   /*Минифицируем CSS*/
       .pipe(gulp.dest('public/css/'));
   });
 
-  gulp.task('minifyJS', function() {  /*Минифицируем JS*/ 
-    return pipeline(
-          gulp.src('app/js/*.js'),
-          uglify(),
-          gulp.dest('public/js/')
-    );
-  });
+gulp.task('minifyJS', function() {  /*Минифицируем JS*/ 
+  return pipeline(
+    gulp.src('app/js/*.js'),
+      uglify(),
+      gulp.dest('public/js/')
+  );
+});
+
+gulp.task('autoPrefixCSS', function() { /*Добавляем автопрефиксы в CSS*/ 
+  return gulp.src('app/css/*.css')
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('public/css/'));
+});
+
