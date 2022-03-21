d9203dc6ac9c03be8b9f36a5f067c564c6738ab4
Mikhail Malashin <begasos1997@gmail.com>
2022-03-21 18:37:44 +0400

    vue

--- vue.js
+++ vue.js
@@ -1,7 +1,6 @@
-
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Привет, VUE '
-    }
-  })
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Привет, Vue!'
+  }
+})
