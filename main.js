13ba387238e5000c1e62888d7f76f36667a8eff7
sham  pun <begasos1997@gmail.com>
2022-07-27 19:13:26 +0400

    Header готов
    Адаптация и  бургер  меню

---
+++ main.js
@@ -0,0 +1,8 @@
+/*Бургер*/ 
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+})
+
