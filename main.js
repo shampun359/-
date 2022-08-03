e202e98f31734fbd81b22027d5064d99faf604b7
sham  pun <begasos1997@gmail.com>
2022-08-03 23:16:28 +0400

    В процессе
    Почти  закончил  первую  страницу

--- main.js
+++ main.js
@@ -6,3 +6,33 @@ menuBtn.addEventListener('click', function(){
     menu.classList.toggle('active');
 })
 
+
+
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+    
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item"); /*Создаём массив из  элементов с item*/ 
+    
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+    
+}
