0e523b6e9eb021c17413efc450d57ccf8dc906db
sham  pun <begasos1997@gmail.com>
2022-10-06 22:00:05 +0400

    Рекурсия

--- грокаю алгоритмы/search.js
+++ грокаю алгоритмы/search.js
@@ -1,6 +1,12 @@
+//Сортировка списка:
+let my_list = [3,5,2,10,8,9,4,6,7,1];
 
-let my_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50];
+my_list.sort(function (a, b) {
+    return a - b;
+});
 
+console.log(my_list);
+//бинарный поиск:
 
 function binary_search(my_list, item){
     
@@ -28,7 +34,7 @@ let a = binary_search(my_list, 2);
 if (a == undefined){
     console.log('Такого элемента нет в списке')
 }else {
-    console.log('элемент ' + my_list[a] + ' существует. Индекс: ' + a);
+    console.log('элемент ' + my_list[a] + ' существует. индекс: ' + a);
 }
 
 
